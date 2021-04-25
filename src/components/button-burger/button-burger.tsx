ddd7c725deb81016a4d78a7ce567a80e4291c13c
Ilya Kiyanov <ikiyanov@yahoo.com>
2021-04-25 05:36:46 +0300

    adds styled components and fixes svg notation

--- src/components/button-burger/button-burger.tsx
+++ src/components/button-burger/button-burger.tsx
@@ -25,7 +25,7 @@ const ButtonBurger: FC<ButtonBurgerProps> = ({ onClick, size = 60 }) => {
       onClick={() => handleClick()}
       style={inlineCSS}
     >
-      <svg viewBox="0 0 100 100">
+      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <path className={`${styles.line} ${styles.line1} ${opened ? styles.openedLine1 : ''}`}
           d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"/>
         <path className={`${styles.line} ${styles.line2} ${opened ? styles.openedLine2 : ''}`} d="M 20,50 H 80"/>
